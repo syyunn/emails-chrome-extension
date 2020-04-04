b081641cb2690864f234d8327c6d4683ce735bfe
zachary <syyun@snu.ac.kr>
2020-04-04 21:20:18 +0900

    Adjust width, height

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{ width: "600px", height: "800px" }}>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
